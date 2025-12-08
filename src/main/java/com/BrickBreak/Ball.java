dfb5100755f0274cb25957ac1d9579d3f7d01614
kaos101-max <watanabedaisuke616@gmail.com>
2025-12-08 19:28:26 +0000

    Yokai

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,15 +1,27 @@
 package com.BrickBreak;
 import java.awt.*;
+import java.awt.Color;
 
 public class Ball {
 	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	
-	//constructor(s):
-	
-	//methods:
+private int x;
+private int y;
+private double changeX;
+private double changeY;
+private int size;
+private Color color; 
 
+	//don't forget you need instance variables:
+public Ball (int x, int y, int changeX, int changeY, Color color, int size){	
+		this.x = x;
+        this.y = y;
+        this.changeX = changeX;
+        this.changeY = changeY;
+        this.color = color;
+        this.size = size;
+    }
+	//constructor(s):
+	ball = new Ball();
+	//methods:
+	
 }
