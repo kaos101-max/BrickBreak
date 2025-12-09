e0348f704995e75731482fd8a05525fa62a55c09
kaos101-max <watanabedaisuke616@gmail.com>
2025-12-09 19:28:29 +0000

    Apple

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -7,15 +7,23 @@ public class Paddle {
 
 
 	//your code here!
-
-
 	
 	//don't forget you need instance variables:
-	
-	
+	private int x;
+	private int y;
+	private int width;
+	private int height;
 	//constructor(s):
-	
+	public Brick (int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
 	//methods:
-	
+	public void draw(Graphic g){
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, width, height);
+	}
 	
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -1,27 +1,29 @@
+
+//Daisuke Watanabe
+// Description: This creates the ball of "Brick Breaker" and its functions
+
 package com.BrickBreak;
 import java.awt.*;
 import java.awt.Color;
 
 public class Ball {
 	//your code here!
-private int x;
-private int y;
-private double changeX;
-private double changeY;
-private int size;
-private Color color; 
+    private int x;
+    private int y;
+    private int size;
 
 	//don't forget you need instance variables:
-public Ball (int x, int y, int changeX, int changeY, Color color, int size){	
+	//constructor(s):
+	public Ball (int x, int y, int size){	
 		this.x = x;
         this.y = y;
-        this.changeX = changeX;
-        this.changeY = changeY;
-        this.color = color;
         this.size = size;
     }
-	//constructor(s):
-	ball = new Ball();
 	//methods:
+    public void draw(Graphics g){
+        g.setColor(Color.CYAN);
+        g.fillOval(x, y size, size);)
+    }
+    public
 	
 }
